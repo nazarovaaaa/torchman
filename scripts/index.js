1fd74aaef37e9af59e191ae69fab6735cfa8ccef
konstantikkov <60280866+konstantikkov@users.noreply.github.com>
2021-12-14 07:14:56 +0300

    full refactor

--- scripts/index.js
+++ scripts/index.js
@@ -29,13 +29,31 @@ const Stages = [
                 left: 270
             }
         ],
+        many: [
+            {
+                top: 200,
+                left: 0
+            },
+            {
+                top: 400,
+                left: 270
+            },
+            {
+                top: 20,
+                left: 200
+            },
+            {
+                top: 200,
+                left: 420
+            }
+        ],
         points: 50,
         background:'url("./assets/map.svg")'
     },
     {
         start:{
-            left: 250,
-            top: 100
+            left: 130,
+            top: 200
         },
         time: 5000,
         points: 100,
@@ -133,6 +151,28 @@ const Stages = [
                 left: 0
             }
         ],
+        many: [
+            {
+                top: 53,
+                left: 90
+            },
+            {
+                top: 100,
+                left: 400
+            },
+            {
+                top: 100,
+                left: 10
+            },
+            {
+                top: 392,
+                left: 377
+            },
+            {
+                top: 347,
+                left: 0
+            }
+        ],
         background:'url("./assets/map_3.svg")'
     }
 ]
@@ -140,10 +180,28 @@ const Stages = [
 // 0 - Двигайтесь по дороге
 // 1 - Фонари необходимо зажигать
 // 2 - Вы пропустили фонарь
+var prev;
 var Notifications = [
-    false,
-    false,
-    false
+    {
+        flag: false,
+        div: `Нажмите на фонарщика, чтобы выбрать его.`
+    },
+    {
+        flag: false,
+        div: `Фонарь гаснет! Зажги его!`
+    },
+    {
+        flag: false,
+        div: `Вести фонарщика необходимо постепенно!`
+    },
+    {
+        flag: false,
+        div: `Вести фонарщика необходимо исключительно по улицам!`
+    },
+    {
+        flag: false,
+        div: `Нажмите два раза на фонарь, чтобы зажечь его`
+    }
 ];
 
 // Тип игры, карта,
@@ -168,7 +226,7 @@ var activeTorch;
 var isTorchManActive;
 
 // Очки, имя, предыдущий/текущий рекорд
-var score, name, record;
+var score, name, record = 0;
 
 // div для карты
 const map = document.getElementById('Map');
@@ -179,14 +237,18 @@ const torchMan = document.getElementById('TorchMan');
 // div'ы для фонарей
 var torchDivs = [];
 
+var town = document.getElementById('Town')
+
+const torchStatus = document.getElementById('TorchStatus')
 // div для меню
 const menu = document.getElementById('Menu');
 
 // div для итогового окна
 const status = document.getElementById('Status');
-console.log(status)
 // div для очков
 const scoreDiv = document.getElementById('Score');
+
+const timer = document.getElementById('Timer')
 
 // div для имени
 const nickname = document.getElementById('Nickname');
@@ -194,9 +256,34 @@ console.log(scoreDiv)
 // div для перехода в menu
 var toMenu = document.getElementById('ToMenu');
 
+var notification = document.getElementById('Notification')
+
+toMenu.onclick = toMenuFunction
+
 torchMan.onclick = () => {
     console.log(11)
-    isTorchManActive = !isTorchManActive
+    disableNotification(0, false)
+    if(!isTorchManActive && activeTorch === -1)
+        interval = setInterval(randomTwinkle, time_to_spawn + 1000)
+    isTorchManActive = true
+}
+
+torchMan.onmouseenter = () => {
+    isTorchManActive?disableNotification(3, false):''
+}
+let time = 0
+
+var time_interval
+
+function startTimer() {
+    time = 0
+    time_interval =     setInterval(function () {
+        let seconds = time%60
+        let minutes = Math.floor(time/60)%60
+        let hour = Math.floor(time/60/60)%60
+        timer.innerText = `${hour} : ${minutes} : ${seconds}`;
+        time++;
+    }, 1000)
 }
 
 
@@ -204,21 +291,93 @@ function switchVisibility(element, mode= false){
     element.className = mode?'':'Hidden'
 }
 
+function setNotification(index, timeout){
+    if(!Notifications[index].flag){
+        insertInnerHtml(notification, Notifications[index].div)
+        switchVisibility(notification,true)
+        if(timeout){
+            setTimeout(()=>{
+                Notifications[index].flag = true
+                switchVisibility(notification)
+            }, timeout)
+        }
+    }
+}
+
+function disableNotification(index, flag=true){
+    Notifications[index].flag = flag
+    switchVisibility(notification)
+}
+
 function insertInnerHtml(element, tags= ""){
     element.innerHTML = tags
 }
 
+function toMenuFunction() {
+    if(Mode === 2){
+        if(score > record){
+            record = score
+            localStorage.setItem('record', record)
+        }
+    }
+    clearInterval(time_interval)
+    isTorchManActive = false
+    toMenu.className='Hidden'
+    status.className='Hidden'
+    clearInterval(interval)
+    refreshMeta()
+    meta()
+}
+
+var torchStatusDivs = []
+
+function generateTorchStatus(){
+    torchStatus.innerHTML = ""
+    torchStatusDivs = []
+    for(let i = 0; i < levelStage.length - 1; i++){
+        const torchStatusDiv = document.createElement('div')
+        torchStatusDiv.className = 'TorchStatusDivInactive'
+        torchStatusDivs.push(torchStatusDiv)
+        torchStatus.append(torchStatusDiv)
+    }
+}
 
 // Функция инициализации карты
 function generateMap() {
     insertInnerHtml(map)
     map.style.backgroundImage = Stages[Stage].background
     map.prepend(torchMan, toMenu)
+    torchMan.className=''
+    map.prepend(timer)
+    startTimer()
+    timer.className=''
+    torchMan.style.top = `${Stages[Stage].start.top}px`
+    torchMan.style.left = `${Stages[Stage].start.left}px`
+    toMenu.className='Button'
     map.prepend(status)
+    map.prepend(notification)
     if(Mode === 2){
         map.prepend(scoreDiv)
+        scoreDiv.className = ''
+        scoreDiv.innerHTML = 0
     }
+    menu.className= 'Hidden'
     map.prepend(nickname, menu)
+
+
+    if(Mode === 0 || Mode === 1){
+        map.prepend(torchStatus)
+        generateTorchStatus()
+    }
+
+    town.onmouseenter = () => {
+        isTorchManActive?setNotification(3):''
+        console.log(1)
+    }
+    town.onmouseleave = () => {
+        console.log(2)
+        isTorchManActive?disableNotification(3, false):''
+    }
 
     roads.forEach((road) =>{
         const roadDiv = document.createElement('div')
@@ -228,6 +387,12 @@ function generateMap() {
         roadDiv.style.width = `${road.right - road.left}px`
         roadDiv.style.height = `${road.bottom - road.top}px`
         roadDiv.onmousemove = moveTorchMan
+        roadDiv.onmouseenter = () => {
+            isTorchManActive?disableNotification(3, false):''
+        }
+        roadDiv.onmouseleave = () => {
+            isTorchManActive?setNotification(3):''
+        }
         map.prepend(roadDiv)
     })
 
@@ -240,6 +405,7 @@ function generateMap() {
         map.append(torchDiv)
         torchDivs.push(torchDiv)
     })
+    map.prepend(town)
 }
 
 // Функция движения фонарщика
@@ -250,6 +416,10 @@ function moveTorchMan(event) {
         currentPosition.left = event.clientX
         torchMan.style.top = `${event.clientY - 23}px`
         torchMan.style.left = `${event.clientX - 26}px`
+        disableNotification(2, false)
+    }
+    else if(isTorchManActive){
+        setNotification(2)
     }
 }
 
@@ -267,13 +437,16 @@ function torchLight(key) {
     if( Math.abs(torch.top + 45 - (currentPosition.top - 23)) < 100
         && Math.abs(torch.left + 30 - (currentPosition.left - 26)) < 100 && activeTorch === key && !stageEnd){
 
-        if(Mode===1){
+        if(Mode===2){
             currentStage--;
         }
         else{
             levelStage[currentStage] = true
         }
 
+        if(Mode === 0 || Mode === 1){
+            torchStatusDivs[currentStage - 1].className='TorchStatusDivActive'
+        }
         torchDivs[key].className = 'Torch'
         activeTorch = -1
 
@@ -289,55 +462,131 @@ function torchLight(key) {
 }
 
 
-
 function refreshMeta () {
     torchDivs = []
     levelStage = [true, ...torches.map((item) => false)];
     stageEnd = false;
     score = 0;
+    isTorchManActive = false;
     activeTorch = -1;
     currentStage = 0;
     currentPosition.top = Stages[Stage].start.top;
     currentPosition.left = Stages[Stage].start.left
 }
 
-// Функция постановки уровня на паузу
-function pause() {
-
+function nextLevel() {
+    switchVisibility(status)
+    if(torches.length === Stages[Stage].few.length){
+        torches = Stages[Stage].many
+    }
+    else{
+        if(Stage !== Stage.length - 1){
+            console.log(1)
+            Stage++;
+        }
+        else{
+            console.log(2)
+            Stage = 0;
+        }
+        torches = Stages[Stage].few
+        roads = Stages[Stage].roads
+    }
+    // refreshMeta()
+    // generateMap()
+    startLevel()
 }
 
-const statusSet = [
-    `<div class="Success">Вы выиграли</div>`,
-    `<div class="Error">Вы проиграли</div>`,
-    `<div class="Success">Новый рекорд!</div>`,
-    `<div class="Error">Предыдущий рекорд:</div>`
-]
+function restartLevel() {
+    switchVisibility(status)
+    startLevel()
+    // refreshMeta()
+    // generateMap()
+}
+
 
 function finishLevel (result) {
+    const statusSet = [
+        `<div class="Success">Вы выиграли</div>
+    <div onclick="nextLevel()"  class="Button">Следующий уровень</div>
+    <div onclick="restartLevel()"  class="Button">Заново</div>
+    <div onclick="toMenuFunction()" class="Button">
+    В меню
+    </div>
+`,
+        `<div class="Error">Вы проиграли</div>
+    <div onclick="restartLevel()"  class="Button">Заново</div>
+    <div onclick="toMenuFunction()" class="Button">
+    В меню
+    </div>
+`,
+        `<div class="Success">Новый рекорд!</div>
+    <div onclick="restartLevel()"  class="Button">Заново</div>
+    <div onclick="toMenuFunction()" class="Button">
+    В меню
+    </div>
+`,
+        `<div class="Error">Предыдущий рекорд:</div>
+    <div onclick="restartLevel()"  class="Button">Заново</div>
+    <div onclick="toMenuFunction()" class="Button">
+    В меню
+    </div>
+`
+    ]
+
+    clearInterval(time_interval)
+
+    toMenu.className='Hidden'
     stageEnd = true;
     switchVisibility(status,true)
     if(result === 0 && (Mode === 0 || Mode === 1)){
         insertInnerHtml(status, statusSet[0])
     }
-    if(result === 1 && (Mode === 0 || Mode === 1)){
+    else if(result === 1 && (Mode === 0 || Mode === 1)){
         insertInnerHtml(status, statusSet[1])
     }
-    if(result === 0 && Mode === 1){
-        insertInnerHtml(status, statusSet[2])
+    else if(Mode === 2 && score > record){
+        record = score
+        localStorage.setItem('record', record)
+        insertInnerHtml(status,`<div class="Success">Новый рекорд!</div>
+    <div>${record}</div>
+    <div onclick="restartLevel()"  class="Button">Заново</div>
+    <div onclick="toMenuFunction()" class="Button">
+    В меню
+    </div>
+`)
     }
-    if(result === 1 && Mode === 1){
-        insertInnerHtml(status, statusSet[3])
+    else if(Mode === 2 && score <= record){
+        insertInnerHtml(status,         `<div class="Error">Предыдущий рекорд:</div>
+    <div>${record}</div>
+    <div class="Error">Текущие очки:</div>
+    <div>${score}</div>
+    <div onclick="restartLevel()"  class="Button">Заново</div>
+    <div onclick="toMenuFunction()" class="Button">
+    В меню
+    </div>
+`)
     }
+    let seconds = time % 60
+    let minutes = Math.floor(time / 60) % 60
+    let hour = Math.floor(time / 60 / 60) % 60
+
+    status.prepend(`Ваше время: ${hour} : ${minutes} : ${seconds}`)
+
     clearInterval(interval)
 }
 
 function randomTwinkle() {
     if(levelStage[currentStage] && !stageEnd){
-        activeTorch = Math.floor(Math.random() * 10) % torches.length;
+        do{
+            activeTorch = Math.floor(Math.random() * 100) % torches.length;
+        }while(activeTorch === prev)
+        prev = activeTorch
         torchDivs[activeTorch].className = 'Torch TorchTwinkling';
+        setNotification(1, 4000)
         currentStage++;
         setTimeout(() => {
             if(!levelStage[currentStage] && !stageEnd){
+                setNotification(3, 3000)
                 finishLevel(1)
             }
         }, time_to_spawn)
@@ -346,10 +595,13 @@ function randomTwinkle() {
 
 // Функция начала уровня
 function startLevel() {
+    clearInterval(interval)
     switchVisibility(menu)
     refreshMeta()
     generateMap()
-    interval = setInterval(randomTwinkle, time_to_spawn + 1000)
+    if(!Notifications[0].flag) {
+        setNotification(0)
+    }
 }
 
 function setMode(mode_){
@@ -377,41 +629,66 @@ function setInterval_(interval_){
     settings(4)
 }
 
+function clear_() {
+    localStorage.clear()
+    location.reload();
+}
+
 // Сет для меню
-const menuSet = [
-    `<div>Режим игры</div>
-     <div onclick="setMode(0)">Определенный уровень</div>
-     <div onclick="setMode(1)">Уровни подряд</div>
-     <div onclick="setMode(2)">Бесконечная игра</div>`,
 
-    `<div>Тип карты</div>
-     <div onclick="setStage(0)">Маленькая</div>
-     <div onclick="setStage(1)">Большая</div>`,
-
-    `<div>Количество фонарей</div>
-     <div onclick="setTorches('few')">Мало</div>
-     <div onclick="setTorches('many')">Много</div>`,
-
-    `<div>Интервал времени</div>
-     <div onclick="setInterval_(3000)">Малый</div>
-     <div onclick="setInterval_(5000)">Большой</div>`,
-
-    `<div onclick="startLevel()">Начать игру</div>`
-]
 
 // Функция создания меню и получение опций
 function settings(step){
+    const menuSet = [
+        `<div>Текущий рекорд в бесконечном режиме: ${record}</div>
+    <div>Режим игры</div>
+     <div class="Button" onclick="setMode(0)">Единственный уровень</div>
+     <div class="Button" onclick="setMode(1)">Уровни подряд</div>
+     <div class="Button" onclick="setMode(2)">Бесконечная игра</div>
+         <div>Опции</div>
+     <div class="Button" onclick="clear_()">Сменить пользователя</div>
+`,
+
+        `<div>Тип карты</div>
+     <div class="Button" onclick="setStage(0)">Маленькая</div>
+     <div class="Button" onclick="setStage(1)">Большая</div>`,
+
+        `<div>Количество фонарей</div>
+     <div class="Button" onclick="setTorches('few')">Мало</div>
+     <div class="Button" onclick="setTorches('many')">Много</div>`,
+
+        `<div>Интервал времени</div>
+     <div class="Button" onclick="setInterval_(3000)">Малый</div>
+     <div class="Button" onclick="setInterval_(5000)">Большой</div>`,
+
+        `<div class="Button" onclick="startLevel()">Начать игру</div>`
+    ]
     menu.innerHTML = menuSet[step]
 }
 
+function setName(event){
+    name = event.target.value
+    insertInnerHtml(nickname, name)
+    localStorage.setItem('nickname', name)
+}
+
 const input = `
-        <input placeholder="введите ваше имя" type="text"/>
-        <div onclick="settings(0)">Подтвердить</div>
+        <input onchange="setName(event)" placeholder="введите ваше имя" type="text"/>
+        <div class="Button" onclick="settings(0)">Подтвердить</div>
 `
 
 function meta(){
     switchVisibility(menu, true)
-    insertInnerHtml(menu, input)
+    name = localStorage.getItem('nickname')
+    const rec_ = localStorage.getItem('record')
+    record = rec_? rec_ : 0
+    if(name == 'null'){
+        insertInnerHtml(menu, input)
+    }
+    else{
+        insertInnerHtml(nickname, name)
+        settings(0)
+    }
 }
 
 meta();
